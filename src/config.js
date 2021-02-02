d56006ce5a73833d6aac8446db278baa12719da4
AJ Markow <alexanderjmarkow@gmail.com>
2021-02-02 14:52:37 -0800

    Add login component. Add config.js with config object.
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 const config = {
   s3: {
-    REGION: process.env('API_REGION'),
+    REGION: process.env.API_REGION,
     BUCKET: 'resourcehubposts'
   },
   apiGateway: {
-    REGION: process.env('API_REGION'),
-    URL: process.env('URL'),
+    REGION: process.env.API_REGION,
+    URL: process.env.URL,
   },
   cognito: {
-    REGION: process.env('API_REGION'),
-    USER_POOL_ID: process.env('USER_POOL_ID'),
-    APP_CLIENT_ID: process.env('CLIENT_ID'),
-    IDENTITY_POOL_ID: process.env('ID_POOL_ID'),
+    REGION: process.env.API_REGION,
+    USER_POOL_ID: process.env.USER_POOL_ID,
+    APP_CLIENT_ID: process.env.CLIENT_ID,
+    IDENTITY_POOL_ID: process.env.ID_POOL_ID,
   },
-}
+};
+
+export default config;
