c3bd3290c09580d2ead8b70023133e9529bd4b36
AJ Markow <alexanderjmarkow@gmail.com>
2021-02-04 10:57:34 -0800

    Add handleFormSubmit function.  Add multiselect list to new post form.  Refactor Programming Language into multiselect list.
    Co-authored-by: Chloe Hellberg <chloe.hellberg@gmail.com>
    Co-authored-by: Ben White <bwhite2140@outlook.com>

--- src/containers/NewPost.js
+++ src/containers/NewPost.js
@@ -6,11 +6,13 @@ import { onError } from "../libs/errorLib";
 import config from "../config";
 import "./NewPost.css";
 import Card from "react-bootstrap/Card";
-import StarRatingComponent from "react-star-rating-component";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 import { useForm } from "react-hook-form";
 import { API } from "aws-amplify";
 
 const contentFilters = ["beginner", "intermediate", "advanced"];
+const options = [1, 2, 3, 4, 5];
 
 export default function NewPost() {
   const file = useRef(null);
@@ -20,19 +22,42 @@ export default function NewPost() {
   const [starRating, setStarRating] = useState(1);
 
   const { register, handleSubmit, errors } = useForm();
-  // const handleSettingContent = (data) => console.log(data);
-  const onSubmit = (data) => console.log(data);
 
-  function validateForm() {
-    return content.length > 0;
+  async function handleFormSubmit(data) {
+    console.log(data);
+    setIsLoading(true);
+
+    try {
+      await createPost({
+        postBlurb: data.postBlurb,
+        postLink: data.postLink,
+        postLanguage: data.postLanguage,
+        postKeywords: data.postKeywords,
+        postRating: data.postRating,
+        attachment: data.attachment,
+      });
+      history.push("/");
+    } catch (e) {
+      onError(e);
+      setIsLoading(false);
+    }
   }
+
+  function createPost(post) {
+    setIsLoading(false);
+    const sentPost = API.post("posts", "/posts", {
+      body: post,
+    });
+    console.log(sentPost);
+    return sentPost;
+  }
+
+  // function validateForm() {
+  //   return content.length > 0;
+  // }
 
   function handleFileChange(event) {
     file.current = event.target.files[0];
-  }
-
-  function handleSettingContent(event) {
-    setContent([...content, event.target.value]);
   }
 
   function onStarClick(nextValue, prevValue, name) {
@@ -52,20 +77,21 @@ export default function NewPost() {
   //   }
 
   //   setIsLoading(true);
+
   // }
 
   return (
     <Card bg="dark" style={{ width: "80%" }} className="p-5 NewPost">
       <div classname="NewPost">
-        <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form onSubmit={handleSubmit(handleFormSubmit)}>
           <Form.Group controlId="PostBlurb">
             <Form.Label className="labels">Describe the Content</Form.Label>
             <Form.Control
               // value={content}
               as="textarea"
-              onChange={(e) => e.target.value}
+              // onChange={(e) => e.target.value}
               ref={register({ required: true })}
-              name="blurb"
+              name="postBlurb"
             />
             {errors.blurb && errors.blurb.type === "required" && (
               <span className="noto" role="alert">
@@ -78,63 +104,64 @@ export default function NewPost() {
             <Form.Control
               // value={content}
               type="text"
-              onChange={handleSettingContent}
+              // onChange={(e) => e.target.value}
               ref={register({ required: true })}
-              name="link"
+              name="postLink"
             />
           </Form.Group>
-          <Form.Group controlId="PostLanguage">
+          <Form.Group controlId="postLanguage">
             <Form.Label className="labels">
               What programming language?
             </Form.Label>
             <Form.Control
               // value={content}
               type="text"
-              onChange={handleSettingContent}
+              // onChange={(e) => e.target.value}
               ref={register({ required: true })}
-              name="labels"
+              name="postLanguage"
             />
           </Form.Group>
           <Form.Group controlId="postKeywords">
             <Form.Label className="labels">Any Keywords?</Form.Label>
-            {contentFilters.map((level, index) => {
-              return (
-                <Form.Label
-                  key={index}
-                  control={
-                    <Form.Check
-                      checked="false"
-                      type="checkbox"
-                      onChange={handleSettingContent}
-                      name={level}
-                      ref={register({ required: true })}
-                    />
-                  }
-                  label={level}
-                />
-              );
-            })}
+            <Form.Control
+              as="select"
+              multiple
+              name="postKeywords"
+              size="3"
+              ref={register}
+            >
+              <option value="Tutorial">Tutorial</option>
+              <option value="Blog Post">Blog Post</option>
+              <option value="Youtube Video">Youtube Video</option>
+              <option value="Podcast">Podcast</option>
+              <option value="Documentation">Documentation</option>
+            </Form.Control>
           </Form.Group>
           <Form.Group controlId="postRating">
             <Form.Label className="labels">Rate it, 1 to 5 Stars.</Form.Label>
             <br></br>
-            <StarRatingComponent
-              name="rating"
-              editing={true}
-              renderStarIcon={() => <span>⭐</span>}
-              starCount={5}
-              value={starRating}
+            <Form.Control
+              as="select"
+              id="rating"
+              name="postRating"
+              size="3"
               ref={register}
-            />
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </Form.Control>
           </Form.Group>
           <Form.Group controlId="Attachment">
             <Form.Label className="labels">Attach a related file</Form.Label>
             <Form.Control
-              onChange={handleFileChange}
+              // onChange={handleFileChange}
               type="file"
               variant="secondary"
               ref={register}
-              name="file"
+              name="attachment"
             />
           </Form.Group>
           <LoaderButton
@@ -143,7 +170,7 @@ export default function NewPost() {
             size="lg"
             variant="primary"
             isLoading={isLoading}
-            disabled={!validateForm()}
+            // disabled={!validateForm()}
           >
             Share it
           </LoaderButton>
